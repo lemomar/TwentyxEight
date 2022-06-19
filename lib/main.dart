0b5ba7144186fb07bdb5fbc8b41f3a88d2c734eb
lemomar <lemomar@users.noreply.github.com>
2022-06-19 16:03:14 +0200

    🎉 Initialized project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TwentyXEight());
+}
+
+class TwentyXEight extends StatelessWidget {
+  const TwentyXEight({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
